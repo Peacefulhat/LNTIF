c67d63d909acbc5a19440e267cbbeb5df4017de5
Peacefulhat <peacefulhat@gmail.com>
2025-03-27 13:59:57 +0530

    update

--- region_based_memory_allocator/arena.c
+++ region_based_memory_allocator/arena.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+
 #define ARENA_SIZE 100 // node of list
 
 typedef enum {
@@ -5,8 +7,7 @@ typedef enum {
   INTEGER,
   FLOAT,
   DOUBLE,
-  UINT,
-  CUSTOM,
+  UINT
 }Types;
 
 typedef struct{
@@ -16,12 +17,19 @@ typedef struct{
 }Region;
   
 typedef struct{
+  Region*begin,*end;
   size_t region_count;
   size_t region_capacity;
   Region*region;
 }Arena;
 
-
+Region *begin=NULL;
+Region *end=NULL;
 void allocate_region(size_t size, size_t capacity,Types t){
-  // ON LUNCH TIME  
+
+}
+
+int main(){
+  
+  return 0;
 }
