14fb62d627e1e451f8caaf910a7a85b425cde4fa
Peacefulhat <peacefulhat@gmail.com>
2025-03-28 12:25:34 +0530

    some updation

--- region_based_memory_allocator/arena.h
+++ region_based_memory_allocator/arena.h
@@ -34,8 +34,8 @@ typedef struct{
   size_t region_capacity;
 }Arena;
 
-Arena* allocate_arena();
-Region* allocate_region(size_t capacity,Types t);
+Arena* alloc_arena();
+Region* alloc_region(size_t capacity,Types t);
 
 
 #endif// ARENA_H
@@ -43,7 +43,7 @@ Region* allocate_region(size_t capacity,Types t);
 #ifdef ARENA_IMP
 
 // alloated area
-Arena* allocate_arena(){
+Arena* alloc_arena(){
   Arena*alloc_space=malloc(sizeof(Arena));
   A_ASSERT(alloc_space!=NULL);
   alloc_space->begin=NULL;
@@ -53,7 +53,7 @@ Arena* allocate_arena(){
   return alloc_space;
 }
 
-Region* allocate_region(size_t capacity,Types t){
+Region* alloc_region(size_t capacity,Types t){
   
   Region*region_block=malloc(sizeof(Region));
   A_ASSERT(region!=NULL);
@@ -111,6 +111,69 @@ Region* allocate_region(size_t capacity,Types t){
  return region_block;
 }
 
+void append_data(Region *block, Type t, void *data,size_t count){
+  A_ASSERT(region!=NULL);
+  Region *new_region=NULL;
+  
+  if(count > block->capacity-block->count){
+    new_region=alloc_region(block->capacity,t);
+    A_ASSERT(new_region!=NULL);
+    block->next=new_region;
+    block = new_region;
+  }
+  
+ switch(t){
+    
+   case CHAR:
+    {
+      region_block->mem_region=malloc(sizeof(char)*capacity);
+      region_block->capacity=capacity;
+      region_block->type=t;
+      region_block->next=NULL;
+    }
+    break;
+    
+  case INT:
+    {
+      region_block->mem_region=malloc(sizeof(int)*capacity);
+      region_block->capacity=capacity;
+      region_block->type=t;
+      region_block->next=NULL;
+    }
+    break;
+    
+      case FLOAT:
+    {
+      region_block->mem_region=malloc(sizeof(float)*capacity);
+      region_block->capacity=capacity;
+      region_block->type=t;
+      region_block->next=NULL;
+    }
+    break;
+    
+  case DOUBLE:
+    {
+      region_block->mem_region=malloc(sizeof(double)*capacity);
+      region_block->capacity=capacity;
+      region_block->type=t;
+      region_block->next=NULL;
+    }
+    break;
+
+  case UINT:
+    {
+      region_block->mem_region=malloc(sizeof(unsigned int)*capacity);
+      region_block->capacity=capacity;
+      region_block->type=t;
+      region_block->next=NULL;
+  }break;
+    
+  default:
+    
+  }
+  
+}
 
 #endif // ARENA_IMP
+
 
