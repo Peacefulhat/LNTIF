854c21a7ef5ee02863c6965d13ff8070c9672cff
Peacefulhat <peacefulhat@gmail.com>
2025-04-17 09:06:04 +0530

    some formatting and updation

--- region_based_memory_allocator/arena.h
+++ region_based_memory_allocator/arena.h
@@ -22,7 +22,7 @@ typedef enum {
 }Types;
 
 
- struct Region{
+struct Region{
   size_t count;
   size_t capacity;
   Types type;
@@ -67,95 +67,92 @@ Arena* alloc_arena(){
   return alloc_space;
 }
 
+// i don't know if break is need or not
 size_t sizeof_types(Types type){
   switch(type){
   case CHAR:
     {
-    return sizeof(char);
+      return sizeof(char);
     }
-    break;
-    
-      case INT:
+      
+  case INT:
     {
-    return sizeof(int);
+      return sizeof(int);
     }
-    break;
-    
+      
   case FLOAT:
     {
-    return sizeof(float);
+      return sizeof(float);
     }
-    break;
-    
-    case DOUBLE:
+      
+  case DOUBLE:
     {
-    return sizeof(double);
+      return sizeof(double);
     }
-    break;
+  
     
   case UINT:
     {
-    return sizeof(unsigned int);
+      return sizeof(unsigned int);
     }
-    break;
+  
   }
   return -1;// indiacting not a valid type
 }
 
 Region* alloc_region(size_t capacity, Types t) {
-    Region* region_block = malloc(sizeof(Region));
-    A_ASSERT(region_block != NULL);
-    region_block->mem_region = malloc(sizeof_types(t) * capacity);
-    A_ASSERT(region_block->mem_region != NULL);
-    
-    region_block->capacity = capacity;
-    region_block->type = t;
-    region_block->count = 0;
-    region_block->next = NULL;
-    return region_block;
+  Region* region_block = malloc(sizeof(Region));
+  A_ASSERT(region_block != NULL);
+  region_block->mem_region = malloc(sizeof_types(t) * capacity);
+  A_ASSERT(region_block->mem_region != NULL);
+  
+  region_block->capacity = capacity;
+  region_block->type = t;
+  region_block->count = 0;
+  region_block->next = NULL;
+  return region_block;
 }
 
 
 static Region *find_compatible_region(Arena *arr, Types t, size_t count) {
-    Region *temp = arr->begin;
-    while (temp != NULL) {
-        if ((temp->capacity - temp->count) >= count && temp->type == t) {
-            return temp;
-        }
-        temp = temp->next;
+  Region *temp = arr->begin;
+  while (temp != NULL) {
+    if ((temp->capacity - temp->count) >= count && temp->type == t) {
+      return temp;
     }
-    return NULL;
+    temp = temp->next;
+  }
+  return NULL;
 }
 
 static Region *create_and_append_region(Arena *arr, Types t, size_t count) {
-    size_t cap = 4 * count;
-    Region *new_region = alloc_region(cap, t);
-    A_ASSERT(new_region != NULL);
+  size_t cap = 4 * count;
+  Region *new_region = alloc_region(cap, t);
+  A_ASSERT(new_region != NULL);
 
-    if (arr->begin == NULL) {
-        arr->begin = arr->end = new_region;
-    } else {
-        arr->end->next = new_region;
-        arr->end = new_region;
-    }
+  if (arr->begin == NULL) {
+    arr->begin = arr->end = new_region;
+  } else {
+    arr->end->next = new_region;
+    arr->end = new_region;
+  }
 
-    return new_region;
+  return new_region;
 }
 
 void append_data(Arena *arr, Types t, void *data, size_t count) {
-    A_ASSERT(arr != NULL);
-    A_ASSERT(data != NULL);
+  A_ASSERT(arr != NULL);
+  A_ASSERT(data != NULL);
+  size_t type_size = sizeof_types(t);
 
-    size_t type_size = sizeof_types(t);
+  Region *region = find_compatible_region(arr, t, count);
+  if (region == NULL) {
+    region = create_and_append_region(arr, t, count);
+  }
 
-    Region *region = find_compatible_region(arr, t, count);
-    if (region == NULL) {
-        region = create_and_append_region(arr, t, count);
-    }
-
-    void *dest = (char *)region->mem_region + region->count * type_size;
-    memcpy(dest, data, count * type_size);
-    region->count += count;
+  void *dest = (char *)region->mem_region + region->count * type_size;
+  memcpy(dest, data, count * type_size);
+  region->count += count;
 }
 
 void region_free(Region* region){
@@ -187,72 +184,78 @@ void arena_free(Arena* arena) {
 }
 
 const char *type_to_string(Types t) {
-    switch (t) {
-        case CHAR: return "CHAR";
-        case INT: return "INT";
-        case FLOAT: return "FLOAT";
-        case DOUBLE: return "DOUBLE";
-        case UINT: return "UINT";
-        default: return "UNKNOWN";
-    }
+  switch (t) {
+  case CHAR: return "CHAR";
+  case INT: return "INT";
+  case FLOAT: return "FLOAT";
+  case DOUBLE: return "DOUBLE";
+  case UINT: return "UINT";
+  default: return "UNKNOWN";
+  }
 }
 
 void region_print(Region* region) {
-    A_ASSERT(region != NULL);
-    printf("Region - Type: %s, Count: %zu, Capacity: %zu\n",
-           type_to_string(region->type), region->count, region->capacity);
+  A_ASSERT(region != NULL);
+  printf("Region - Type: %s, Count: %zu, Capacity: %zu\n",
+         type_to_string(region->type), region->count, region->capacity);
 
     size_t i;
     switch (region->type) {
-        case CHAR: {
-            char *data = (char *)region->mem_region;
-            for (i = 0; i < region->count; i++) {
-                printf("  [%zu] = '%c'\n", i, data[i]);
-            }
-            break;
-        }
-        case INT: {
-            int *data = (int *)region->mem_region;
-            for (i = 0; i < region->count; i++) {
-                printf("  [%zu] = %d\n", i, data[i]);
-            }
-            break;
-        }
-        case FLOAT: {
-            float *data = (float *)region->mem_region;
-            for (i = 0; i < region->count; i++) {
-                printf("  [%zu] = %f\n", i, data[i]);
-            }
-            break;
-        }
-        case DOUBLE: {
-            double *data = (double *)region->mem_region;
-            for (i = 0; i < region->count; i++) {
-                printf("  [%zu] = %lf\n", i, data[i]);
-            }
-            break;
-        }
-        case UINT: {
-            unsigned int *data = (unsigned int *)region->mem_region;
-            for (i = 0; i < region->count; i++) {
-                printf("  [%zu] = %u\n", i, data[i]);
-            }
-            break;
-        }
-        default:
-            printf("  Unknown data type.\n");
+    case CHAR: {
+      char *data = (char *)region->mem_region;
+      for (i = 0; i < region->count; i++){
+        printf("  [%zu] = '%c'\n", i, data[i]);
+      }
+    }
+      break;
+    }
+    
+    case INT: {
+      int *data = (int *)region->mem_region;
+      for (i = 0; i < region->count; i++) {
+        printf("  [%zu] = %d\n", i, data[i]);
+      }
+      break;
+    }
+      
+    case FLOAT: {
+      float *data = (float *)region->mem_region;
+      for (i = 0; i < region->count; i++) {
+        printf("  [%zu] = %f\n", i, data[i]);
+      }
+      break;
+    }
+      
+    case DOUBLE: {
+      double *data = (double *)region->mem_region;
+      for (i = 0; i < region->count; i++) {
+        printf("  [%zu] = %lf\n", i, data[i]);
+      }
+      break;
+    }
+      
+    case UINT: {
+      unsigned int *data = (unsigned int *)region->mem_region;
+      for (i = 0; i < region->count; i++) {
+        printf("  [%zu] = %u\n", i, data[i]);
+      }
+      break;
+    }
+      
+    default:
+      printf("  Unknown data type.\n");
     }
 }
 
 void arena_print(Arena* arena) {
-    A_ASSERT(arena != NULL);
-    Region* temp = arena->begin;
-    int index = 0;
-    while (temp != NULL) {
-        printf("Region #%d:\n", index++);
-        region_print(temp);
-        temp = temp->next;
-    }
+  A_ASSERT(arena != NULL);
+  Region* temp = arena->begin;
+  int index = 0;
+  while (temp != NULL) {
+    printf("Region #%d:\n", index++);
+    region_print(temp);
+    temp = temp->next;
+  }
 }
 
 #endif // ARENA_IMP
